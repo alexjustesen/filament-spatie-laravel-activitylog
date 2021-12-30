28fe7544715a53bf0c5d570b2e9ca10be296b1e1
Ryan Chandler <ryangjchandler@gmail.com>
2021-12-30 18:14:08 +0000

    init

--- src/FilamentSpatieLaravelActivitylog.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace RyanChandler\FilamentSpatieLaravelActivitylog;
-
-class FilamentSpatieLaravelActivitylog
-{
-}
--- src/FilamentSpatieLaravelActivitylogServiceProvider.php
+++ src/FilamentSpatieLaravelActivitylogServiceProvider.php
@@ -4,22 +4,12 @@ namespace RyanChandler\FilamentSpatieLaravelActivitylog;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use RyanChandler\FilamentSpatieLaravelActivitylog\Commands\FilamentSpatieLaravelActivitylogCommand;
 
 class FilamentSpatieLaravelActivitylogServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
-            ->name('filament-spatie-laravel-activitylog')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_filament-spatie-laravel-activitylog_table')
-            ->hasCommand(FilamentSpatieLaravelActivitylogCommand::class);
+            ->name('filament-spatie-laravel-activitylog');
     }
 }
