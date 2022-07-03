26050d2cf76349f87d8d550ae8fdbc9e91d8d14f
Pathros <latlovs-github@yahoo.com>
2022-07-02 21:50:53 -0500

    Added Spanish translation

---
+++ resources/lang/es/activity.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'causer_type'  => 'Tipo de causante',
+    'causer_id'    => 'Id del causante',
+    'subject_type' => 'Tipo de objeto sometido',
+    'subject_id'   => 'Id del objeto sometido',
+    'description'  => 'Descripción',
+    'attributes'   => 'Atributos / valor(es) actual(es) o nuevo(s)',
+    'old'          => 'Valor(es) anterior(es)',
+    'subject'      => "Asunto",
+    'logged_at'    => "Registrado el",
+    'has_subject'  => "Tiene objeto sometido",
+    'label'        => 'Actividad',
+    'plural_label' => 'Registros de actividad'
+];
