e20a9fe0e413c036eccb57ea5b694aae51021808
Hachi Abderrahmane Ben Salem <iha2007hachi@gmail.com>
2023-02-26 14:57:42 +0100

    Added Arabic translation

---
+++ resources/lang/ar/activity.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'causer_type' => 'نوع المستخدم',
+    'causer_id' => 'معرف المستخدم',
+    'subject_type' => 'نوع الموضوع',
+    'subject_id' => 'معرف الموضوع',
+    'description' => 'الوصف',
+    'attributes' => 'السمات',
+    'old' => 'القيمة القديمة',
+    'subject' => 'الموضوع',
+    'logged_at' => 'وقت التسجيل',
+    'log' => 'سجل',
+    'plural_log' => 'سجلات',
+    'has_subject' => 'يحتوي على موضوع',
+    'label' => 'النشاط',
+    'plural_label' => 'النشاطات',
+];
